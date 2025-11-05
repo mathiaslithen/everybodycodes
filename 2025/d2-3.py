8c4aa7f0b95b1fa5e3bc2c654b24a0dbecb975dd
Mathias <mathias.lithen@gmail.com>
2025-11-05 21:37:05 +0200

    d2 also works with complex nums

--- 2025/d2-3.py
+++ 2025/d2-3.py
@@ -1,46 +1,24 @@
-import math
-
-D = (100000, 100000)
-A = [-4531, 67892]
-
+A = (-4531, 67892)
+d = 100000+100000j
+lim = 1000000
 points = [
-    (x, y)
+    complex(x, y)
     for y in range(A[1], A[1]+1001)
     for x in range(A[0], A[0]+1001)
 ]
 
 
-def add(a, b):
-    x1, y1 = a
-    x2, y2 = b
-    return (x1+x2, y1+y2)
-
-
-def mul(a, b):
-    x1, y1 = a
-    x2, y2 = b
-    return (x1*x2-y1*y2, x1*y2+y1*x2)
-
-
 def div(a, b):
-    x1, y1 = a
-    x2, y2 = b
-    _, x = math.modf(x1/x2)
-    _, y = math.modf(y1/y2)
-    return (int(x), int(y))
+    return complex(int(a.real / b.real), int(a.imag / b.imag))
 
 
-S = set()
-
+ans = 0
 for p in points:
-    res = (0, 0)
+    res = 0j
     for _ in range(100):
-        res = mul(res, res)
-        res = div(res, D)
-        res = add(res, p)
-        if not (-1000000 <= res[0] <= 1000000 and -1000000 <= res[1] <= 1000000):
+        res = div(res*res, d) + p
+        if any(abs(i) > lim for i in (res.real, res.imag)):
             break
     else:
-        S.add(p)
-
-print(len(S))
+        ans += 1
+print(ans)
