5cf77edb4b1c48bd72f7aa6d6560dcada0f4e16a
Mathias Lithen <mathias.lithen@gmail.com>
2025-11-06 08:25:49 +0200

    cleanup

--- 2025/d3.py
+++ 2025/d3.py
@@ -1,13 +1,18 @@
 import collections
-import sys
 
-with open(sys.argv[1]) as f:
-    G = list(map(int, f.read().strip().split(',')))
 
+def get_input(fn):
+    with open(fn) as f:
+        return list(map(int, f.read().strip().split(',')))
+
+
+G = get_input('3-1.in')
 print('A', sum(set(G)))
+
+G = get_input('3-2.in')
 print('B', sum(list(sorted(set(G)))[:20]))
 
-
+G = get_input('3-3.in')
 c = collections.Counter(G)
 ans = 0
 while any(c.values()):
